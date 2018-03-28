c7b2be718fac06aaee9240e57d99f7a33d00784c
jonth <jonth@10.0.0.28>
2018-03-27 17:25:34 -0700

    testing if the change will push to the web

--- JavaTest1/src/JMW1966/HelloWorld.java
+++ JavaTest1/src/JMW1966/HelloWorld.java
@@ -2,7 +2,7 @@ package JMW1966;
 
 public class HelloWorld {
 	
-	// This is similar to a #define in C or C++
+	// This is similar to a #define in C or C++ for those who have programmed in those languages
 	private static final String Output = new String("Hello World");
 	
 	HelloWorld() {
