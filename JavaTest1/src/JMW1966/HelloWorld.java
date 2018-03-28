7d12a5e9f46156452c4b67b4369e01eb1bc86e6b
jonth <jonth@10.0.0.28>
2018-03-27 17:28:05 -0700

    More SCM tests

--- JavaTest1/src/JMW1966/HelloWorld.java
+++ JavaTest1/src/JMW1966/HelloWorld.java
@@ -2,11 +2,11 @@ package JMW1966;
 
 public class HelloWorld {
 	
-	// This is similar to a #define in C or C++ for those who have programmed in those languages
+	// This is similar to a #define in C or C++ for those who have programmed in those languages.
 	private static final String Output = new String("Hello World");
 	
 	HelloWorld() {
-		System.out.println(Output);
+		System.out.println(Output); // pop out a message 
 		
 	}
 
