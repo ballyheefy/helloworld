8ad289d0c70cb2dd70cc0b8b2a25daa642016f1c
jonth <jonth@10.0.0.28>
2018-03-27 17:16:22 -0700

    Basline changes

---
+++ JavaTest1/src/JMW1966/HelloWorld.java
@@ -0,0 +1,13 @@
+package JMW1966;
+
+public class HelloWorld {
+	
+	// This is similar to a #define in C or C++
+	private static final String Output = new String("Hello World");
+	
+	HelloWorld() {
+		System.out.println(Output);
+		
+	}
+
+}
---
+++ JavaTest1/src/JMW1966/HelloWorldTester.java
@@ -0,0 +1,12 @@
+package JMW1966;
+
+public final class HelloWorldTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HelloWorld hw = new HelloWorld(); 
+
+	}
+
+}
