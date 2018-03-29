c01d8056eb25e68f480fbdc3e6f98390c3471235
jonth <jonth@10.0.0.28>
2018-03-28 18:59:55 -0700

    Scanner games and JUNIT

---
+++ JavaTest1/src/com/jonathanwalsh/scanner/ScannerCapabilities.java
@@ -0,0 +1,42 @@
+package com.jonathanwalsh.scanner;
+
+
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ScannerCapabilities {
+	
+	
+	// default constructor 
+	public ScannerCapabilities() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	
+	/*
+	 * This takes a prompt and returns and integer returning the status of the scan the user of this function must evaluate 
+	 * the scanstate variable to understand if this worked 
+	 */
+	public int getIntWithPrompt(Scanner s, String prompt) {
+		
+		System.out.print(prompt);
+		try {
+			if(s.hasNextInt()) {
+				int x = s.nextInt();
+				return x; 
+			}
+		} catch (IllegalStateException  e) {
+			System.err.println("Illegal state");
+			e.printStackTrace();
+		} catch (NoSuchElementException e)  {
+			System.err.println("No Such element");
+			e.printStackTrace();
+		} finally {
+			// nothing to do here left for sake of completeness 
+		}
+		return 0;
+	}
+
+}
---
+++ JavaTest1/src/com/jonathanwalsh/scanner/ScannerCapabilitiesTest1.java
@@ -0,0 +1,26 @@
+package com.jonathanwalsh.scanner;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+// next step is to make these parmaterized tests 
+public class ScannerCapabilitiesTest1 extends TestCase {
+	
+	
+
+	public ScannerCapabilitiesTest1 () {
+		
+	}
+	@Test
+	public void test1() {
+		int x;
+		ScannerCapabilities sc = new ScannerCapabilities();
+		
+		x = sc.getIntWithPrompt(new Scanner(System.in),"enter an integer : ");
+		System.out.println("value entered : "+ x);
+		assert x > 0;
+	}
+}
